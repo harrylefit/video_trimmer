225867960e72d1af530caa43fbf87abeb4a5a240
Harry Le <harryle.fit@gmail.com>
2022-02-22 19:26:23 +0700

    Update trimmer.dart

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:path/path.dart';
 
